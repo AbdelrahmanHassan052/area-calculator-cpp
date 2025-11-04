224c4245a1b7acb024ec2b1789aef78c016be155
Abdelrahman Hassan <Abdelrahman_M01592@cic-cairo.edu.eg>
2025-11-05 00:50:15 +0200

    Add square and rhombus area calculations
    Updated area calculator to include square and rhombus calculations.

--- Area of shapes.cpp
+++ Area of shapes.cpp
@@ -1,28 +1,41 @@
 //Program : Area calculator (shapes)
 //Author: Abdelrahman Hassan
 //Date : November 2025
-//Description : Calculates the area of rectangle , triangle and circle using if-else and while loop
+//Description : Calculates the area of square , rectangle , rhombus , triangle and circle using if-else and while loop
 
 #include <iostream>
 using namespace std;
 int main()
 {
     int choice;
-   double Length , Width , area , radius , height ,base ;
+   double  Side ,Length , Width , area , radius , height ,base ,diagonal1 ,diagonal2;
+   const double PI =3.14159;
     char again ='y';
     while (again =='y' || again =='Y'){
-    cout <<"==========Area calculator===========""\n";
+    cout <<"======================================\n";
+    cout <<"       AREA CALCULATOR PROGRAM      \n";
+    cout <<"======================================\n";
    cout << "chose a shape :""\n";
-   cout << "1. Rectangle ""\n";
-   cout << "2. Triangle""\n";
-   cout << "3. circle""\n";
-   cout << "======================================""\n";
-   cout << "Enter your choice (1 - 3) : ";
+   cout << "1. Square\n";
+   cout << "2. Rectangle \n";
+   cout << "3. Rhombus \n";
+   cout << "4. Triangle\n";
+   cout << "5. circle\n";
+   cout << "======================================\n";
+   cout << "Enter your choice (1 - 5) : ";
    cin >> choice;
+   
+   
+   if (choice ==1){
+       cout << "Enter the side of the square : ";
+       cin >> Side ;
+       area = Side * Side;
+       cout <<"The area of the square = "<<area <<"\n";
+   }
    
       
    
-   if (choice ==1){
+   else if (choice ==2){
        cout << "Enter the length of the rectangle : ";
        cin >> Length ;
        cout<<"Enter the width of the rectangle : ";
@@ -31,7 +44,16 @@ int main()
        cout<<"The area of the rectangle = " <<area <<"\n";
        
    }
-   else if (choice ==2){
+   
+   else if (choice ==3){
+       cout << "Enter the diagonal 1 of the rhombus : ";
+       cin >> diagonal1;
+       cout <<"Enter the diagonal 2 of the rhombus : ";
+       cin >> diagonal2;
+       area = ( diagonal1 * diagonal2 )/ 2;
+       cout << "The Area of the rhombus = "<<area <<"\n";
+   }
+   else if (choice ==4){
        cout << "Enter the base of the triangle : ";
        cin >> base ;
        cout << "Enter the height of the triangle : ";
@@ -39,20 +61,20 @@ int main()
        area = 0.5 * base * height;
        cout << "The area of the triangle ="<<area <<"\n";
    }
-   else if (choice ==3){
+   else if (choice ==5){
        cout << "Enter radius of the circle : ";
        cin >> radius ;
-       area = radius * radius * 3.14159;
+       area = radius * radius * PI ;
        cout <<"The area of the circle ="<<area <<"\n";
    }
    else{
-       cout << "Invaild choice! please enter 1 , 2 , or 3."<<"\n";
+       cout << "Invaild choice! please enter 1 , 2 , 3 , 4 or 5."<<"\n";
    }
    cout << "\n Do you want to calculate another shape? (y/n):";
    cin >> again;
    
    }
-   cout << "Thanks for using the area calculator! Good bye 👋 ""\n";
+   cout << "Thanks for using the area calculator! Good bye 👋 \n";
    
     return 0;
 }
